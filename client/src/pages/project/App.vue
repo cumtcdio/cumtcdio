98b75171ce57927a248b8c29fba0d9c4a66193c8
Bruce <wx_3b2d694f889b4d2691e457b0dfe87975@git.code.tencent.com>
2019-03-19 18:34:33 +0800

    home  and  project
    home  and  project

---
+++ client/src/pages/project/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="project">
+    project
+    <router-link to="/">project</router-link>
+  </div>
+</template>
+
+<style>
+
+</style>
--- client/vue.config.js
+++ client/vue.config.js
@@ -11,6 +11,12 @@ module.exports = {
             template: 'public/cdio.html',
             filename: 'cdio.html',
             title: '详情'
+        },
+        project: {
+            entry: 'src/pages/project/main.js',
+            template: 'public/project.html',
+            filename: 'project.html',
+            title: '项目制介绍'
         }
     }
 }
